ca04fb32eb52e1b974cbd311c7b961dc0858c673
Jasper Niemeyer <jani5714@github.com>
2026-02-11 17:01:12 -0700

    update models with chrono timestamps

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,14 +1,18 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize)]
+use crate::types::PhoneNumber;
+
+#[derive(Debug, Serialize, FromRow)]
 pub struct WhitelistEntry {
     pub id: Uuid,
-    pub phone_number: String,
+    pub phone_number: PhoneNumber,
     pub name: String,
     pub reason: Option<String>,
-    pub created_at: String,
-    pub expires_at: Option<String>,
+    pub created_at: DateTime<Utc>,
+    pub expires_at: Option<DateTime<Utc>>,
     pub is_permanent: bool,
 }
 
@@ -17,7 +21,7 @@ pub struct CreateWhitelistEntry {
     pub phone_number: String,
     pub name: String,
     pub reason: Option<String>,
-    pub expires_at: Option<String>,
+    pub expires_at: Option<DateTime<Utc>>,
     pub is_permanent: Option<bool>,
 }
 
